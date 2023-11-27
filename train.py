2fd29ad7fce4e33c41a01c290d57e6bf05b0a686
Zellenon <zellenon@gmail.com>
2023-11-27 15:45:52 -0600

    added old training code

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from deepxml.dataset import MultiLabelDataset
 from deepxml.meshprobenet import CorNetMeSHProbeNet, MeSHProbeNet, TreeNetMeSHProbeNet
 from deepxml.models import GPipeModel, Model
 from deepxml.xmlcnn import XMLCNN, CorNetXMLCNN
-from global_config import Path, data_dir, results_dir
+from global_config import DatasetConfig, Path, data_dir, results_dir
 
 model_dict = {
     "AttentionXML": AttentionXML,
@@ -27,8 +27,49 @@ model_dict = {
     "CorNetXMLCNN": CorNetXMLCNN,
 }
 
+def train_model(cfg: DatasetConfig, model_cnf, refiner):
+    model, model_name, data_name = None, model_cnf["name"], cfg.name
+    model_path = results_dir / Path("models") / f"{model_name}-{data_name}"
 
-def train_model(cfg, model_cnf, refiner):
+    mlb = get_mlb(
+        cfg.label_binarizer,
+        None,
+    )
+    labels_num = len(mlb.classes_)
+
+    emb_init = cfg.emb_init
+    logger.info(F'Model Name: {model_name}')
+
+    logger.info('Loading Training and Validation Set')
+    train_x, train_labels = get_data(cfg.data['train'])
+    # random_state = data_cnf['valid'].get('random_state', 1240)
+    random_state = 1240
+    train_x, valid_x, train_labels, valid_labels = train_test_split(train_x, train_labels,
+                                                                        test_size=cfg.valid_size,random_state=random_state)
+    mlb = get_mlb(cfg.label_binarizer, np.hstack((train_labels, valid_labels)))
+    train_y, valid_y = mlb.transform(train_labels), mlb.transform(valid_labels)
+    labels_num = len(mlb.classes_)
+    logger.info(F'Number of Labels: {labels_num}')
+    logger.info(F'Size of Training Set: {len(train_x)}')
+    logger.info(F'Size of Validation Set: {len(valid_x)}')
+
+    logger.info('Training')
+    train_loader = DataLoader(MultiLabelDataset(train_x, train_y),
+                              cfg.batch_size, shuffle=True, num_workers=4)
+    valid_loader = DataLoader(MultiLabelDataset(valid_x, valid_y, training=True),
+                              cfg.batch_size, num_workers=4)
+    if 'gpipe' not in model_cnf:
+        model = Model(network=model_dict[model_name], labels_num=labels_num, model_path=model_path, emb_init=emb_init,
+                      **cfg.model, **model_cnf['model'])
+    else:
+        model = GPipeModel(model_name, labels_num=labels_num, model_path=model_path, emb_init=emb_init,
+                           **cfg.model, **model_cnf['model'])
+    # model.train(train_loader, valid_loader, **model_cnf['train'])
+    model.train(train_loader, valid_loader)
+    logger.info('Finish Training')
+
+
+def train_model2(cfg, model_cnf, refiner):
     model, model_name, data_name = None, model_cnf["name"], cfg.name
     model_path = results_dir / Path("models") / f"{model_name}-{data_name}"
 
