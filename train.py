a9579e84e4087d939d96f3e7abc0001227bef1a9
Zellenon <zellenon@gmail.com>
2024-01-21 17:14:00 -0600

    cleanup, adding cornet back

--- train.py
+++ train.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from db import ChromaLoader
 from deepxml.attentionxml import AttentionXML, CorNetAttentionXML
 from deepxml.bertxml import BertXML, CorNetBertXML
+from deepxml.cornet import CorNet, CorNetWrapper
 from deepxml.data_utils import get_data, get_mlb, get_word_emb
 from deepxml.dataset import MultiLabelDataset
 from deepxml.meshprobenet import CorNetMeSHProbeNet, MeSHProbeNet, TreeNetMeSHProbeNet
@@ -31,30 +32,24 @@ model_dict = {
 def train_model(cfg: DatasetConfig, model_cnf, refiner):
     model, model_name, data_name = None, model_cnf["name"], cfg.name
     model_path = results_dir / Path("models") / f"{model_name}-{data_name}"
-
     mlb = get_mlb(
         cfg.label_binarizer,
         None,
     )
     labels_num = len(mlb.classes_)
-
     emb_init = cfg.emb_init
-    logger.info(f"Model Name: {model_name}")
+
 
     logger.info("Loading Training and Validation Set")
     train_x, train_labels = get_data(cfg.data["train"])
-    # random_state = data_cnf['valid'].get('random_state', 1240)
     random_state = 1240
     train_x, valid_x, train_labels, valid_labels = train_test_split(
         train_x, train_labels, test_size=cfg.valid_size, random_state=random_state
     )
-    # mlb = get_mlb(cfg.label_binarizer, np.hstack((train_labels, valid_labels)))
     train_y, valid_y = mlb.transform(train_labels), mlb.transform(valid_labels)
     labels_num = len(mlb.classes_)
-    import ipdb
 
-    # ipdb.set_trace()
-
+    logger.info(f"Model Name: {model_name}")
     logger.info(f"Number of Labels: {labels_num}")
     logger.info(f"Size of Training Set: {len(train_x)}")
     logger.info(f"Size of Validation Set: {len(valid_x)}")
@@ -86,74 +81,10 @@ def train_model(cfg: DatasetConfig, model_cnf, refiner):
             **cfg.model,
             **model_cnf,
         )
+    if refiner == "CorNet":
+        model = CorNetWrapper(model)
     model.train(train_loader, valid_loader, **model_cnf)
     # model.train(train_loader, valid_loader)
     logger.info("Finish Training")
 
 
-def train_model2(cfg, model_cnf, refiner):
-    model, model_name, data_name = None, model_cnf["name"], cfg.name
-    model_path = results_dir / Path("models") / f"{model_name}-{data_name}"
-
-    mlb = get_mlb(
-        cfg.label_binarizer,
-        None,
-    )
-    labels_num = len(mlb.classes_)
-
-    train_set = ChromaLoader(cfg, mlb)
-    train_ids = train_set.train_ids()
-    np.random.shuffle(train_set.train_ids())
-    # TODO: Add support for configured valid splits
-    valid_count = int(len(train_ids) * 0.2)
-    train_x, valid_x = train_ids[:-valid_count], train_ids[valid_count:]
-
-    train_loader = DataLoader(
-        train_set,
-        cfg.batch_size,
-        shuffle=True,
-        num_workers=4,
-    )
-    valid_loader = DataLoader(
-        ChromaLoader(cfg, mlb, training=False),
-        cfg.batch_size,
-        num_workers=4,
-    )
-    logger.info(f"Model Name: {model_name}")
-
-    logger.info("Loading Training and Validation Set")
-    # random_state = data_cnf["valid"].get("random_state", 1240)
-
-    labels_num = len(mlb.classes_)
-    logger.info(f"Number of Labels: {labels_num}")
-    logger.info(f"Size of Training Set: {len(train_x)}")
-    logger.info(f"Size of Validation Set: {len(valid_x)}")
-
-    logger.info("Training")
-    # train_loader = DataLoader(
-    #     MultiLabelDataset(train_x, train_y),
-    #     data_cnf["train"]["batch_size"],
-    #     shuffle=True,
-    #     num_workers=4,
-    # )
-    if "gpipe" not in model_cnf:
-        model = Model(
-            network=model_dict[model_name],
-            labels_num=len(mlb.classes_),
-            model_path=model_path,
-            emb_init=cfg.emb_init,
-            **cfg.model,
-            **model_cnf["model"],
-        )
-    else:
-        model = GPipeModel(
-            model_name,
-            labels_num=labels_num,
-            model_path=model_path,
-            emb_init=cfg.emb_init,
-            # **data_cnf["model"],
-            **model_cnf["model"],
-        )
-    model.train(train_loader, valid_loader, **cfg["train"])
-    # model.train(train_loader, valid_loader)
-    logger.info("Finish Training")
