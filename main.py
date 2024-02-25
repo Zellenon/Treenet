957914b980852022b738dc85cdc7fd3db15a5f42
Zellenon <zellenon@gmail.com>
2024-02-25 13:36:22 -0600

    split functionality into menu submodule

---
+++ main.py
@@ -0,0 +1,65 @@
+
+from consolemenu import ConsoleMenu, MultiSelectMenu
+from consolemenu.items import SubmenuItem
+from consolemenu.items import FunctionItem
+from yaml import safe_load
+import menu
+from global_config import AppConfig, result_log_dir
+
+def pre_process(dataset_cfg_path, _, _2):
+    from global_config import DatasetConfig
+    from preprocess import process
+
+    process(DatasetConfig(safe_load(open(dataset_cfg_path))),
+            vocab_size=500000,
+            max_len=500)
+
+
+def train(dataset_cfg_path, model_cfg_path, refiner):
+    from global_config import DatasetConfig
+    from train import train_model
+
+    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
+    model_params = safe_load(open(model_cfg_path))
+
+    # train_model(dataset_params, model_params, refiner[refiner_choice])
+    train_model(dataset_params, model_params, refiner)
+
+
+def test(dataset_cfg_path, model_cfg_path, refiner):
+    from global_config import DatasetConfig
+    from test import predict
+    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
+    model_params = safe_load(open(model_cfg_path))
+    predict(dataset_params, model_params, refiner)
+
+
+def evaluate(dataset_cfg_path, model_cfg_path, refiner):
+    from global_config import DatasetConfig
+    from evaluation import evaluate
+
+    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
+    model_params = safe_load(open(model_cfg_path))
+
+    results = evaluate(dataset_params, model_params, refiner)
+
+    with open(
+                  result_log_dir / (model_params["name"] + "-" + dataset_params.name + "-" +
+                                     refiner + ".txt"), 'w'
+                                     ) as logfile:
+        logfile.write("\n\n".join([f"{secname}:\n" +"\n".join(
+            [f"{metric}: {score}" for metric,score in sec.items()]
+            ) for secname, sec in results.items()]))
+
+
+
+menu.app_config.tasks = {
+    "PRE": pre_process,
+    "TRAIN": train,
+    "TEST": test,
+    "EVAL": evaluate
+}
+
+if __name__ == "__main__":
+    settings = menu.get_settings()
+    settings.exec()
--- menu.py
+++ menu.py
@@ -4,7 +4,6 @@ from consolemenu import ConsoleMenu, MultiSelectMenu
 from consolemenu.items import SubmenuItem
 from consolemenu.items import FunctionItem
 from yaml import safe_load
-
 from global_config import AppConfig, result_log_dir
 
 app_config = AppConfig()
@@ -67,60 +66,7 @@ def task_selector():
     return menu
 
 
-def pre_process(dataset_cfg_path, _, _2):
-    from global_config import DatasetConfig
-    from preprocess import process
-
-    process(DatasetConfig(safe_load(open(dataset_cfg_path))),
-            vocab_size=500000,
-            max_len=500)
-
-
-def train(dataset_cfg_path, model_cfg_path, refiner):
-    from global_config import DatasetConfig
-    from train import train_model
-
-    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
-    model_params = safe_load(open(model_cfg_path))
-
-    # train_model(dataset_params, model_params, refiner[refiner_choice])
-    train_model(dataset_params, model_params, refiner)
-
-
-def test(dataset_cfg_path, model_cfg_path, refiner):
-    from global_config import DatasetConfig
-    from test import predict
-    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
-    model_params = safe_load(open(model_cfg_path))
-    predict(dataset_params, model_params, refiner)
-
-
-def evaluate(dataset_cfg_path, model_cfg_path, refiner):
-    from global_config import DatasetConfig
-    from evaluation import evaluate
-
-    dataset_params = DatasetConfig(safe_load(open(dataset_cfg_path)))
-    model_params = safe_load(open(model_cfg_path))
-
-    results = evaluate(dataset_params, model_params, refiner)
-
-    with open(
-                  result_log_dir / (model_params["name"] + "-" + dataset_params.name + "-" +
-                                     refiner + ".txt"), 'w'
-                                     ) as logfile:
-        logfile.write("\n\n".join([f"{secname}:\n" +"\n".join(
-            [f"{metric}: {score}" for metric,score in sec.items()]
-            ) for secname, sec in results.items()]))
-
-
-app_config.tasks = {
-    "PRE": pre_process,
-    "TRAIN": train,
-    "TEST": test,
-    "EVAL": evaluate
-}
-
-if __name__ == "__main__":
+def get_settings():
     menu = ConsoleMenu("Main Menu", app_config.all_info, exit_option_text="Execute Tasks")
 
     dataset_menu = SubmenuItem("Select Datasets", dataset_selector())
@@ -143,5 +89,5 @@ if __name__ == "__main__":
 
     menu.start()
     menu.join()
-    app_config.exec()
+    return app_config
 
